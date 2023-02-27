d1b8310c02368e1d38c65fd3728d4a310390c77d
evalott100 <evalott100@users.noreply.github.com>
2023-02-27 16:26:40 +0000

    deploy: 8d3a47610dbd9b327023673e0332308b1aa8f9d1

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev53+g8a24bc0',
+    VERSION: '0.1.dev54+g8d3a476',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
