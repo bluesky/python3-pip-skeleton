e06116a4845dd592e9d5e7c543cf47a475b915df
evalott100 <evalott100@users.noreply.github.com>
2023-02-27 11:47:03 +0000

    deploy: d63925453835aedaa7c770bbd6eefcbde0aafd68

--- 1_convert_pip_skeleton_to_meet_the_bluesky_requirements/_static/documentation_options.js
+++ 1_convert_pip_skeleton_to_meet_the_bluesky_requirements/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev55+g6d68b8e',
+    VERSION: '0.1.dev57+gd639254',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
