f37de742b0911a2cf77ff6fca5de177d0d952061
evalott100 <evalott100@users.noreply.github.com>
2023-04-13 09:31:57 +0000

    deploy: 53731720c8deba3ac6a43def0db057f1a12f42e3

--- publish_docs_to_bluesky_project_io/_static/documentation_options.js
+++ publish_docs_to_bluesky_project_io/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev57+ged216cb',
+    VERSION: '0.1.dev56+g5373172',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
